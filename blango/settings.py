53aeb7a7f70362c2dfece3876bc913e6205d7663
Ekkapan Chanduaykit <ee.ekkapan@gmail.com>
2022-03-03 09:54:46 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -83,6 +83,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg'
     ]
 
     REST_FRAMEWORK = {
@@ -94,6 +95,13 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
     SITE_ID = 1
